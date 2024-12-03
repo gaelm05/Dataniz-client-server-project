8bf35fd5555f668ae5562a989e56a560750d55c5
MoreRice48 <mauricee.diaz@gmail.com>
2024-12-02 20:58:18 -0800

    Client is fixed

--- TCPClient.py
+++ TCPClient.py
@@ -13,7 +13,8 @@ try:
     print(f'Connected to {serverIP}')
     connected = True
     print('!!Type "end chat" to quit!!')
-    someData = input("Enter a message you want to send\n:")
+    print('!Enter "help" for commands!')
+    someData = input("Enter a command\n:")
 
 except socket.error as error:
     print(f'Connection failed: {error}')
@@ -21,17 +22,34 @@ except socket.error as error:
 
 maxBytesToReceive = 4096
 
+def help_user():
+    print("Try these commands")
+    print("what is the average moisture inside my kitchen fridge in the past three hours?\n"
+          "what is the average water consumption per cycle in my smart dishwasher?\n"
+          "which device consumed more electricity among my three IoT devices?")
 
 while connected & (someData != 'end chat'):
-    myTCPSocket.send(bytearray(str(someData), encoding = 'utf-8'))
+    serverResponse = None
 
-    serverResponse = myTCPSocket.recv(maxBytesToReceive)
+    if someData == "help":
+        help_user()
+    elif someData == "what is the average moisture inside my kitchen fridge in the past three hours?":
+        myTCPSocket.send(bytearray(str('average mositure'), encoding = 'utf-8'))
+    elif someData == "what is the average water consumption per cycle in my smart dishwasher?":
+        myTCPSocket.send(bytearray(str('average water used'), encoding = 'utf-8'))
+    elif someData == "which device consumed more electricity among my three IoT devices?":
+        myTCPSocket.send(bytearray(str('most electricity used'), encoding = 'utf-8'))
+    else:
+        print('Sorry, this query cannot be processed. Try typing "help"')
+
+    #myTCPSocket.send(bytearray(str(someData), encoding = 'utf-8'))
+
+    #serverResponse = myTCPSocket.recv(maxBytesToReceive)
 
     if serverResponse != None:
         print(f'{serverIP}:')
         print(serverResponse.decode('utf-8'))
-    print("You:")
-    someData = input()
+    someData = input(":")
     if someData == None:
         someData = ' '
 
